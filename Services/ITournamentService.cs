50c0a0fe3c63007b03a799fdc90775f7351712d8
Martin Andersson <martin.andersson@me.com>
2026-02-21 21:01:20 +0100

    created ItournamentService and TournamentService

---
+++ Services/ITournamentService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface ITournamentService
+    {
+        Task<List<Tournament>> GetAllAsync(string? search);
+        Task<Tournament?> GetByIdAsync(int id);
+        Task<Tournament> CreateAsync(Tournament tournament);
+        Task<bool> UpdateAsync(int id, Tournament updated);
+        Task<bool> DeleteAsync(int id);
+    }
+}
