4789a8a6da7c886014650e7d6ba8637679fd1d56
Martin Andersson <martin.andersson@me.com>
2026-02-21 21:18:54 +0100

    Created FutureDateAttributes and TournamentCreateDto and TournamentResponseDto and TournamentUpdateDto

---
+++ Dtos/FutureDateAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Dtos;
+
+public class FutureDateAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not DateTime dt)
+            return ValidationResult.Success;
+
+        // Date får inte vara i dåtid
+        if (dt.Date < DateTime.UtcNow.Date)
+            return new ValidationResult("Date får inte vara i dåtid.");
+
+        return ValidationResult.Success;
+    }
+}
